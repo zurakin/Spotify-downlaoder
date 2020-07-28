89270658e68f729ae3b2ffed86e0b76d6f0f3bfa
ILYASS AZIRAR <52070629+zurakin@users.noreply.github.com>
2020-07-28 01:02:57 +0100

    first functional version

--- scraper.py
+++ scraper.py
@@ -13,15 +13,14 @@ class Miner():
         # r = requests.get(self.link)
         # c = r.content
         self.driver.get(self.link)
-        time.sleep(2)
+        # time.sleep(2)
         c = self.driver.page_source
         cont = bs(c, "html.parser")
         all = cont.find('ol', {"class":"tracklist"})
-        titles = all.find_all('div', {"class":"tracklist-name ellipsis-one-line"})
-        artists = all.find_all('a', {"class":"tracklist-row__artist-name-link"})
-        for t,a in zip(titles, artists):
-            title = t.text.strip()
-            artist = a.text.strip()
+        all = all.find_all('li')
+        for sg in all:
+            title = sg.find('div', {"class":"tracklist-name ellipsis-one-line"}).text.strip()
+            artist = sg.find('a', {"class":"tracklist-row__artist-name-link"}).text.strip()
             self.titles_list.append([title,artist])
         # for s in songs_list:
             # title = s.find('div', {"class":"tracklis-name ellipsis-one-line"}).text.strip()
--- youtube.py
+++ youtube.py
@@ -1,4 +1,4 @@
-import youtube_dl, time
+import youtube_dl, time, os
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 
@@ -13,12 +13,24 @@ class Downloader():
             search_link = "https://www.youtube.com/results?search_query="+q
             vid_link = "https://www.youtube.com"+self.get_video_link(search_link)
             download_links.append(vid_link)
-            print(vid_link)
+        self.download(download_links)
 
     def get_video_link(self,search_link):
         self.driver.get(search_link)
-        time.sleep(1)
+        # time.sleep(1)
         req = self.driver.page_source
         cont = bs(req, 'html.parser')
         anch = cont.find('a',{"id":"video-title"})
         return anch['href']
+
+    def download(self, download_links):
+        ydl_opts = {'format': 'bestaudio/best',
+        'postprocessors': [{'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',}]}
+        try :
+            os.chdir('downloads')
+        except:
+            os.mkdir('downloads')
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(download_links)
